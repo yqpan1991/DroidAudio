818bfa03330fbdbd20de20cd574ba2f22faee219
PandaPan <280853071@qq.com>
2017-12-18 08:25:18 +0800

    1. 格式化文件 2. fix bug: 开始录制时,文件如果为空,没有标记mHasError = true; 3. 在录音结束时,如果没有真正释放播放器,便不需要通知和设置mIsRecording=true(fix bug: 在快速点击界面时,快速多次点击录音会导致此问题)

--- app/src/main/java/droidaudio/apollo/edus/com/droidaudio/record/audio/MediaRecordWrapper.java
+++ app/src/main/java/droidaudio/apollo/edus/com/droidaudio/record/audio/MediaRecordWrapper.java
@@ -15,33 +15,31 @@ import droidaudio.apollo.edus.com.droidaudio.Utils.MainLooper;
  * 自己记录状态,不主动进行停止,让外面操作,保证数据流的唯一
  * <br>踩过的坑<br>
  * <p>
- *
- *     1. 在开始录音后,快速的结束录音时,发现总是会回调onException,发现的现象: 在设置了onError,并且在stop时没有移除setOnErrorListener,
- *     然后开始MediaRecorder的stop()和release()操作时,会发生这样的情况
- *     解决办法: 因为我们的MediaRecorder只是使用一次,因而在stop()时,应该先移除回调,再进行其他的操作,
- *     可能是MediaRecorder的状态有问题,所以我们也可以不调用MediaRecorder的stop()操作,
- *     直接release(),但是看google的状态机,这样做不好,所以还是先移除监听,再stop,然后release是最好的做法
- *
+ * <p>
+ * 1. 在开始录音后,快速的结束录音时,发现总是会回调onException,发现的现象: 在设置了onError,并且在stop时没有移除setOnErrorListener,
+ * 然后开始MediaRecorder的stop()和release()操作时,会发生这样的情况
+ * 解决办法: 因为我们的MediaRecorder只是使用一次,因而在stop()时,应该先移除回调,再进行其他的操作,
+ * 可能是MediaRecorder的状态有问题,所以我们也可以不调用MediaRecorder的stop()操作,
+ * 直接release(),但是看google的状态机,这样做不好,所以还是先移除监听,再stop,然后release是最好的做法
+ * <p>
  * </p>
  * Created by panda on 2017/12/17.
  */
 
 public class MediaRecordWrapper extends BaseRecord {
 
-    private static final boolean LOG_ENABLE = false;
+    private static final boolean LOG_ENABLE = true;
 
     private String TAG = this.getClass().getSimpleName();
 
-    private boolean mIsRecording;
     private String mFilePath;
     private Context mContext;
     private MediaRecorder mRecorder;
+    private boolean mIsRecording;
     private boolean mHasError;
 
-
-
-    public MediaRecordWrapper(Context context){
-        if(context == null){
+    public MediaRecordWrapper(Context context) {
+        if (context == null) {
             throw new RuntimeException("MediaRecordWrapper context cannot be null");
         }
         mContext = context.getApplicationContext();
@@ -51,7 +49,9 @@ public class MediaRecordWrapper extends BaseRecord {
     public void startRecord() {
         //检测是否合法,开始了录制,则不再开始
         //开启线程
-        if(isRecording()){
+        log("startRecord()");
+        if (isRecording()) {
+            log("---startRecord when is recording, just return");
             return;
         }
         mIsRecording = true;
@@ -68,7 +68,8 @@ public class MediaRecordWrapper extends BaseRecord {
         //2. 构造录音的参数
         //3. 开始录制
         String filePath = makeFilePath();
-        if(TextUtils.isEmpty(filePath)){
+        if (TextUtils.isEmpty(filePath)) {//保护文件路径的操作,正常来讲是肯定存在的
+            mHasError = true;
             notifyOnRecordException(filePath, IRecordListener.ERROR_FILE_GENERATE_ERROR, null);
             return;
         }
@@ -96,21 +97,21 @@ public class MediaRecordWrapper extends BaseRecord {
         } catch (IOException e) {
             e.printStackTrace();
             mHasError = true;
-            log("IOException:"+e.toString());
+            log("IOException:" + e.toString());
             unbindInnerRecorderListener();
-            notifyOnRecordException(mFilePath, IRecordListener.ERROR_RECORD_START_EXCEPTION, " start exception:"+e.toString());
-        }catch (RuntimeException e){
+            notifyOnRecordException(mFilePath, IRecordListener.ERROR_RECORD_START_EXCEPTION, " start exception:" + e.toString());
+        } catch (RuntimeException e) {
             e.printStackTrace();
             mHasError = true;
-            log("RuntimeException:"+e.toString());
+            log("RuntimeException:" + e.toString());
             unbindInnerRecorderListener();
-            notifyOnRecordException(mFilePath, IRecordListener.ERROR_RECORD_START_EXCEPTION, " start exception:"+e.toString());
-        }catch (Exception e){
+            notifyOnRecordException(mFilePath, IRecordListener.ERROR_RECORD_START_EXCEPTION, " start exception:" + e.toString());
+        } catch (Exception e) {
             e.printStackTrace();
             mHasError = true;
-            log("Exception:"+e.toString());
+            log("Exception:" + e.toString());
             unbindInnerRecorderListener();
-            notifyOnRecordException(mFilePath, IRecordListener.ERROR_RECORD_START_EXCEPTION, " start exception:"+e.toString());
+            notifyOnRecordException(mFilePath, IRecordListener.ERROR_RECORD_START_EXCEPTION, " start exception:" + e.toString());
         }
     }
 
@@ -120,12 +121,12 @@ public class MediaRecordWrapper extends BaseRecord {
             mHasError = true;
             log("onError");
             unbindInnerRecorderListener();
-            notifyOnRecordException(mFilePath, IRecordListener.ERROR_INNER_AUDIO_RECORD, "inner audio record error,code:"+what+",extra:"+extra);
+            notifyOnRecordException(mFilePath, IRecordListener.ERROR_INNER_AUDIO_RECORD, "inner audio record error,code:" + what + ",extra:" + extra);
         }
     };
 
     private void unbindInnerRecorderListener() {
-        if(mRecorder != null){
+        if (mRecorder != null) {
             mRecorder.setOnErrorListener(null);
             mRecorder.setOnInfoListener(null);
         }
@@ -134,7 +135,7 @@ public class MediaRecordWrapper extends BaseRecord {
     private MediaRecorder.OnInfoListener mOnInfoListener = new MediaRecorder.OnInfoListener() {
         @Override
         public void onInfo(MediaRecorder mr, int what, int extra) {
-            log("onInfo:what:"+what+",extra:"+extra);
+            log("onInfo:what:" + what + ",extra:" + extra);
         }
     };
 
@@ -142,44 +143,59 @@ public class MediaRecordWrapper extends BaseRecord {
     @Override
     public void stopRecord() {
         //检测合法性,如果没有开始,则直接返回
-        if(!isRecording()){
+        log("stopRecord()");
+        if (!isRecording()) {
+            log("---stopRecord when is recording, just return");
             return;
         }
         RecordUtils.getSingleRecordTask().start(new Runnable() {
             @Override
             public void run() {
-                //1. 如果开启了录音,关闭录音即可
-                //2. 如果在录制过程中,出现了错误,不再通知onRecordStop,即错误和异常二者只通知一次
-                //3. 其他情况下,通知结束
-                releaseRecordInner();
-                checkNotifyRecordStop();
-                MainLooper.instance().postRunnable(new Runnable() {
-                    @Override
-                    public void run() {
-                        mIsRecording = false;
-                    }
-                });
-
+                stopRecordInner();
             }
         });
     }
 
+    private void stopRecordInner() {
+        //1. 如果开启了录音,关闭录音即可
+        //2. 如果在录制过程中,出现了错误,不再通知onRecordStop,即错误和异常二者只通知一次
+        //3. 其他情况下,通知结束
+        log("stopRecordInner()");
+        boolean realReleased = releaseRecordInner();//由于stopRecord 可能会被外面多次调用,并且并且异步操作,因而需要做校验,是否是真正的释放了,这样才能向外通知
+        if (realReleased) {
+            checkNotifyRecordStop();
+            MainLooper.instance().postRunnable(new Runnable() {
+                @Override
+                public void run() {
+                    mIsRecording = false;
+                }
+            });
+        }
+    }
+
 
     private void checkNotifyRecordStop() {
-        if(mHasError){
+        if (mHasError) {
             return;
         }
         notifyOnStopRecord(mFilePath);
     }
 
-    private void releaseRecordInner() {
-        if(mRecorder != null){
+    /**
+     * 释放内部的录音器
+     * @return true 存在播放器并且真正的释放了 false 不存在播放器的信息
+     */
+    private boolean releaseRecordInner() {
+        if (mRecorder != null) {
             //必须先移除监听,否则在start()后,快速的stop()时,MediaRecorder的错误状态信息可能会回调出来
             unbindInnerRecorderListener();
             mRecorder.reset();
             mRecorder.release();
             mRecorder = null;
+            return true;
         }
+        log("stopped where recorder not exists");
+        return false;
     }
 
     @Override
@@ -190,13 +206,13 @@ public class MediaRecordWrapper extends BaseRecord {
     }
 
     private String makeFilePath() {
-        if(mContext == null){
+        if (mContext == null) {
             return null;
         }
         String dir = null;
-        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             dir = mContext.getExternalCacheDir().getAbsolutePath();
-        }else{
+        } else {
             dir = mContext.getFilesDir().getAbsolutePath();
         }
         File file = new File(dir);
@@ -204,9 +220,9 @@ public class MediaRecordWrapper extends BaseRecord {
         return dir + File.separator + "record-" + System.currentTimeMillis() + ".amr";
     }
 
-    private void log(String info){
-        if(LOG_ENABLE){
-            Log.e(TAG, "[MediaRecordWrapper]"+info);
+    private void log(String info) {
+        if (LOG_ENABLE) {
+            Log.e(TAG, "[MediaRecordWrapper]" + info);
         }
     }
 }
