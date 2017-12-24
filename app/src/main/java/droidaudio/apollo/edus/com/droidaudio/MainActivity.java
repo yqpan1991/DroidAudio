a10119544ec9791825b9da57e45466983b55e73d
PandaPan <280853071@qq.com>
2017-12-24 18:20:33 +0800

    删除v1版本的数据

--- app/src/main/java/droidaudio/apollo/edus/com/droidaudio/MainActivity.java
+++ app/src/main/java/droidaudio/apollo/edus/com/droidaudio/MainActivity.java
@@ -12,30 +12,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.bt_media).setOnClickListener(this);
         findViewById(R.id.bt_media_v2).setOnClickListener(this);
-        findViewById(R.id.bt_audio).setOnClickListener(this);
         findViewById(R.id.bt_pcm).setOnClickListener(this);
-        findViewById(R.id.bt_media_test).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.bt_media:
-                startActivity(new Intent(this, MediaActivity.class));
-                break;
             case R.id.bt_media_v2:
                 startActivity(new Intent(this, MediaV2Activity.class));
                 break;
-            case R.id.bt_audio:
-                startActivity(new Intent(this, AudioActivity.class));
-                break;
             case R.id.bt_pcm:
                 startActivity(new Intent(this, PcmActivity.class));
-                break;
-            case R.id.bt_media_test:
-                startActivity(new Intent(this, MediaPlayerWrapperTestActivity.class));
                 break;
         }
     }
